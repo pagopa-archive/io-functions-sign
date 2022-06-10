ab5a0b6199d0fb5b3d56e8748c5a9883d7873438
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-06-10 12:05:55 +0200

    update subscription id decoder

--- src/ui/http.ts
+++ src/ui/http.ts
@@ -5,14 +5,12 @@ import { header } from "@pagopa/handler-kit/lib/http";
 import { SubscriptionId } from "../signature-request/subscription";
 
 export const requireSubscriptionId = flow(
-  header("x-io-subscription-id"),
-  E.fromOption(
-    () => new Error(`missing required "x-io-subscription-id" header`)
-  ),
+  header("x-subscription-id"),
+  E.fromOption(() => new Error(`Missing X-Subscription-Id header`)),
   E.chain(
     flow(
       SubscriptionId.decode,
-      E.mapLeft(() => new Error("invalid subscription id"))
+      E.mapLeft(() => new Error("Invalid Subscription Id"))
     )
   )
 );
