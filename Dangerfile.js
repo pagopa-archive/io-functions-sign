76d4b18b8a67e1efb210217e38a46eaab1394df3
Francesco Grauso <grausof@gmail.com>
2022-07-06 22:28:18 +0200

    ci: integrate danger
    Reviewed-By: @lucacavallaro @gunzip 
    Refs: SFEQS-818

---
+++ Dangerfile.js
@@ -0,0 +1,17 @@
+const customRules = require("@pagopa/danger-plugin").default;
+
+const recordScope = {
+  projectToScope: {
+    SFEQS: "Firma con IO"
+  },
+  tagToScope: {
+    development: "Development",
+    backend: "Backend",
+    dependency: "Dependency",
+  },
+  minLenPrDescription: 10,
+  updateLabel: false,
+  updateTitle: false
+};
+
+customRules(recordScope);
