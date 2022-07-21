2bebb2a7882d4fcf5a4702b5da7aa3a8476882d1
grausof <grausof@gmail.com>
2022-07-21 13:25:54 +0200

    Add service method

--- src/infra/services/send-message.ts
+++ src/infra/services/send-message.ts
@@ -4,46 +4,30 @@ import * as TE from "fp-ts/lib/TaskEither";
 
 import { UrlFromString } from "@pagopa/ts-commons/lib/url";
 import { pipe, flow } from "fp-ts/lib/function";
+
 import { sequenceS } from "fp-ts/lib/Apply";
-import { config } from "../../app/config";
 import { NewMessage } from "../../ui/api-models/NewMessage";
 import { MessageCreatedResponse } from "../../ui/api-models/MessageCreatedResponse";
-import { makeHttpRequest, RequestMessageHeaders } from "./http";
+import { makeHttpRequest } from "./http";
+import { basePath, headers } from "./service";
 
-const messageForUserUrl = (fiscalCode: FiscalCode) =>
+const makeMessagePat = (fiscalCode: string) =>
   pipe(`/api/v1/messages/${fiscalCode}`, UrlFromString.decode);
-
-const makeRequestParameters = (fiscalCode: FiscalCode) =>
-  pipe(
-    config,
-    E.chain((config) =>
-      pipe(
-        sequenceS(E.Apply)({
-          base_path: pipe(config.service.serviceBasePath, UrlFromString.decode),
-          path: pipe(fiscalCode, messageForUserUrl),
-        }),
-        E.bimap(
-          () => new Error("Invalid service parameters"),
-          ({ base_path, path }) => {
-            const headers: RequestMessageHeaders = {
-              "content-type": "application/json",
-              "Ocp-Apim-Subscription-Key":
-                config.service.serviceSubscriptionKey,
-            };
-            return { base_path, path, headers };
-          }
-        )
-      )
-    )
-  );
 
 export const sendMessage = (fiscalCode: FiscalCode) => (body: NewMessage) =>
   pipe(
-    fiscalCode,
-    makeRequestParameters,
+    sequenceS(E.Apply)({
+      basePath,
+      headers,
+      path: pipe(
+        fiscalCode,
+        makeMessagePat,
+        E.mapLeft(() => new Error("Invalid path"))
+      ),
+    }),
     TE.fromEither,
-    TE.chain(({ base_path, path, headers }) =>
-      makeHttpRequest(base_path.href)({
+    TE.chain(({ basePath, headers, path }) =>
+      makeHttpRequest(basePath.href)({
         body: JSON.stringify(body),
         method: "POST",
         path: path.href,
---
+++ src/infra/services/service.ts
@@ -0,0 +1,28 @@
+import * as E from "fp-ts/lib/Either";
+
+import { UrlFromString } from "@pagopa/ts-commons/lib/url";
+import { pipe } from "fp-ts/lib/function";
+import { config } from "../../app/config";
+import { RequestMessageHeaders } from "./http";
+
+export const basePath = pipe(
+  config,
+  E.chain((config) =>
+    pipe(
+      config.service.serviceBasePath,
+      UrlFromString.decode,
+      E.mapLeft(() => new Error("Invalid service parameters"))
+    )
+  )
+);
+
+export const headers = pipe(
+  config,
+  E.map((config) => {
+    const headers: RequestMessageHeaders = {
+      "content-type": "application/json",
+      "Ocp-Apim-Subscription-Key": config.service.serviceSubscriptionKey,
+    };
+    return headers;
+  })
+);
