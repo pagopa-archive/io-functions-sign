261975103309f94cf159c096c79bf961be5b0677
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2022-07-24 21:03:57 +0200

    prettier

--- Dangerfile.js
+++ Dangerfile.js
@@ -2,7 +2,7 @@ const customRules = require("@pagopa/danger-plugin").default;
 
 const recordScope = {
   projectToScope: {
-    SFEQS: "Firma con IO"
+    SFEQS: "Firma con IO",
   },
   tagToScope: {
     development: "Development",
@@ -11,7 +11,7 @@ const recordScope = {
   },
   minLenPrDescription: 10,
   updateLabel: false,
-  updateTitle: false
+  updateTitle: false,
 };
 
 customRules(recordScope);
