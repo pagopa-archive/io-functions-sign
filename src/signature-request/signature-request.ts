4eb3cc6f0c287abdf3b2af2a8e3b8a76233857b4
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-07-25 14:06:23 +0200

    enhance signature request

--- src/signature-request/signature-request.ts
+++ src/signature-request/signature-request.ts
@@ -12,12 +12,14 @@ export const SignatureRequestId = t.string;
 
 export const SignatureRequestStatus = t.keyof({
   DRAFT: null,
+  WAIT_FOR_SIGNATURE: null,
 });
+
+export type SignatureRequestStatus = t.TypeOf<typeof SignatureRequestStatus>;
 
 export const SignatureRequest = t.intersection([
   t.type({
     id: SignatureRequestId,
-    status: SignatureRequestStatus,
     signerId: SignerId,
     subscriptionId: SubscriptionId,
     productId: ProductId,
@@ -26,12 +28,21 @@ export const SignatureRequest = t.intersection([
   Timestamps,
 ]);
 
+export const status = (request: SignatureRequest): SignatureRequestStatus =>
+  request.documents.every((document) => document.url && document.url.length > 0)
+    ? "WAIT_FOR_SIGNATURE"
+    : "DRAFT";
+
 export type SignatureRequest = t.TypeOf<typeof SignatureRequest>;
 
 export type GetSignatureRequest = (
   id: SignatureRequest["id"],
   subscriptionId: SignatureRequest["subscriptionId"]
 ) => TaskEither<Error, Option<SignatureRequest>>;
+
+export type UpsertSignatureRequest = (
+  request: SignatureRequest
+) => TaskEither<Error, SignatureRequest>;
 
 export type AddSignatureRequest = (
   request: SignatureRequest
