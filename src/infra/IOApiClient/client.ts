cf293f4fc3d9efa0df9bcb7fcf0f254e4e8360fe
grausof <grausof@gmail.com>
2022-08-03 10:41:25 +0200

    Add fetch timeout

--- src/infra/IOApiClient/client.ts
+++ src/infra/IOApiClient/client.ts
@@ -1,16 +1,31 @@
+import { agent } from "@pagopa/ts-commons";
+import {
+  AbortableFetch,
+  setFetchTimeout,
+  toFetch,
+} from "@pagopa/ts-commons/lib/fetch";
+
 import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 
 import { createClient } from "@pagopa/io-functions-services-sdk/client";
-import nodeFetch from "node-fetch";
+import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import { config } from "../../app/config";
+
+const httpApiFetch = agent.getHttpFetch(process.env);
+const abortableFetch = AbortableFetch(httpApiFetch);
 
 export const ioApiClient = pipe(
   config,
   E.map((config) =>
     createClient<"SubscriptionKey">({
       baseUrl: config.ioapi.serviceBasePath,
-      fetchApi: nodeFetch as unknown as typeof fetch,
+      fetchApi: toFetch(
+        setFetchTimeout(
+          config.ioapi.serviceRequestTimeout as Millisecond,
+          abortableFetch
+        )
+      ) as unknown as typeof fetch,
       withDefaults: (op) => (params) =>
         op({
           ...params,
--- src/infra/IOApiClient/config.ts
+++ src/infra/IOApiClient/config.ts
@@ -1,14 +1,17 @@
 import * as t from "io-ts";
+import * as E from "fp-ts/Either";
 import * as RE from "fp-ts/ReaderEither";
-import { pipe } from "fp-ts/function";
-
+import { flow, pipe } from "fp-ts/function";
 import { sequenceS } from "fp-ts/lib/Apply";
+
+import { IntegerFromString } from "@pagopa/ts-commons/lib/numbers";
 
 import { readFromNodeEnv } from "../../app/env";
 
 export const IOApiClientConfig = t.type({
   serviceBasePath: t.string,
   serviceSubscriptionKey: t.string,
+  serviceRequestTimeout: t.number,
 });
 
 export type IOApiClientConfig = t.TypeOf<typeof IOApiClientConfig>;
@@ -23,4 +26,14 @@ export const getIOApiClientConfigFromEnvironment: RE.ReaderEither<
     RE.orElse(() => RE.right("https://api.io.pagopa.it"))
   ),
   serviceSubscriptionKey: readFromNodeEnv("IOApiSubscriptionKey"),
+  serviceRequestTimeout: pipe(
+    readFromNodeEnv("ServiceRequestTimeout"),
+    RE.chainEitherK(
+      flow(
+        IntegerFromString.decode,
+        E.mapLeft(() => new Error("Invalid ServiceRequestTimeout"))
+      )
+    ),
+    RE.orElse(() => RE.right(10000))
+  ),
 });
