26da9aab8e74edc8fd1e3ddf9c1fcac9497965b3
grausof <grausof@gmail.com>
2022-08-25 15:17:28 +0200

    add webjobs-config

---
+++ src/infra/azure/storage/webjobs-config.ts
@@ -0,0 +1,28 @@
+import * as t from "io-ts";
+
+import * as RE from "fp-ts/ReaderEither";
+
+import { sequenceS } from "fp-ts/lib/Apply";
+
+import { pipe } from "fp-ts/lib/function";
+
+import { readFromNodeEnv } from "../../../app/env";
+
+export const WebJobsStorageConfig = t.type({
+  connectionString: t.string,
+  queueName: t.string,
+});
+
+export type WebJobStorageConfig = t.TypeOf<typeof WebJobsStorageConfig>;
+
+export const getWebJobsStorageConfigFromEnvironment: RE.ReaderEither<
+  NodeJS.ProcessEnv,
+  Error,
+  WebJobStorageConfig
+> = sequenceS(RE.Apply)({
+  connectionString: readFromNodeEnv("AzureWebJobsStorage"),
+  queueName: pipe(
+    readFromNodeEnv("QueueName"),
+    RE.orElse(() => RE.right("waiting-for-signature"))
+  ),
+});
