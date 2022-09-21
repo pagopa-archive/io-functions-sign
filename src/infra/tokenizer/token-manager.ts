8a5e2579b6b5bebc0ea6e8a455b7af0f20b1b588
Filippo Tenaglia <filippo.tenaglia@pagopa.it>
2022-09-21 12:30:12 +0200

    Update src/infra/tokenizer/token-manager.ts
    Co-authored-by: Danilo Spinelli <gunzip@users.noreply.github.com>

--- src/infra/tokenizer/token-manager.ts
+++ src/infra/tokenizer/token-manager.ts
@@ -2,7 +2,7 @@ import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
 
-import { FiscalCode } from "@pagopa/io-functions-services-sdk/FiscalCode";
+import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 import { config } from "../../app/config";
 import { tokenizerApiClient } from "./client";
 
